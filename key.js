6a98d30d4753a443391ffe5d93eda1257b0c2db3
Robin DuBois <46360947+Robin-DuBois@users.noreply.github.com>
2019-06-04 15:05:52 -0600

    Create key.js

---
+++ key.js
@@ -0,0 +1,11 @@
+var asciify = require("asciify");
+const chalk = require('chalk');
+
+asciify(`Loading...`, function (err, res) {
+    console.log(chalk.yellow.bgRed.bold(res));
+});
+
+exports.spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+};
